73b37fd9bff3be0bf95829be1f5ea36c01d024ae
Mikkel Dollas <141632210+mikkeldollassmidt@users.noreply.github.com>
2025-03-05 10:56:09 +0100

    hurtig

--- my-app/app/productActionPage.jsx
+++ my-app/app/productActionPage.jsx
@@ -11,7 +11,7 @@ const fetchProductDetails = (id) => {
   return dummyProducts[id] || { title: "Ukendt produkt", imageUrl: "", category: "Ukendt", description: "Ingen beskrivelse tilgængelig" };
 };
 
-const ProductActionPage = () => {
+const productActionPage = () => {
   const router = useRouter();
   const { id } = useLocalSearchParams();
   const [product, setProduct] = useState(null);
@@ -44,4 +44,110 @@ const ProductActionPage = () => {
   );
 };
 
-export default ProductActionPage;
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: "white",
+    height: "100%",
+    width: "100%",
+  },
+  productImageContainer: {
+    maxWidth: "100%",
+    overflow: "hidden",
+    borderBottomLeftRadius: 30,
+    borderBottomRightRadius: 30,
+  },
+  productImage: {
+    height: 400,
+  },
+  buttonContainer: {
+    position: "absolute",
+    bottom: 0,
+    justifyContent: "center",
+    alignItems: "center",
+    width: "100%",
+    height: 100,
+    padding: 20,
+  },
+  loanButton: {
+    backgroundColor: "#08B5CF",
+    width: "100%",
+    padding: 13,
+    borderRadius: 10,
+    position: "absolute",
+    top: 15,
+  },
+  loanButtonText: {
+    fontSize: 17,
+    fontWeight: "500",
+    color: "white",
+    textAlign: "center",
+  },
+  deliverButton: {
+    backgroundColor: "#D0F6FC",
+    width: "100%",
+    padding: 13,
+    borderRadius: 10,
+    position: "absolute",
+    top: 15,
+  },
+  deliverButtonText: {
+    fontSize: 17,
+    fontWeight: "500",
+    color: "#08B6CF",
+    textAlign: "center",
+  },
+  textContainer: {
+    padding: 20,
+  },
+  headerContainer: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    flexWrap: "wrap",
+  },
+  headerText: {
+    fontSize: 17,
+    fontWeight: "bold",
+    flexShrink: 1,
+    flexWrap: "wrap",
+    color: "#363636",
+    maxWidth: "75%",
+  },
+  typeText: {
+    fontSize: 15,
+    color: "#08B5CF",
+    fontWeight: "500",
+    paddingHorizontal: 15,
+    paddingVertical: 3,
+    maxHeight: 25,
+    textAlign: "center",
+    backgroundColor: "#D0F6FC",
+    borderRadius: 8,
+  },
+  descContainer: {
+    marginTop: 10,
+  },
+  descHeader: {
+    fontSize: 14,
+    fontWeight: 600,
+    color: "#363636",
+  },
+  descSubheader: {
+    fontSize: 13,
+    color: "#363636",
+  },
+  goBackIcon: {
+    height: 15,
+    width: 15,
+    transform: [{ rotate: "90deg" }],
+  },
+  goBackIconBox: {
+    backgroundColor: "white",
+    padding: 10,
+    position: "absolute",
+    zIndex: 999,
+    borderRadius: 8,
+    left: 20,
+    top: 20,
+  },
+});
+export default productActionPage;
